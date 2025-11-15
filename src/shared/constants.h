b2f410461f1a8d56ab337ab51f6551bca56b3aa6
randomGuyi <arthurdambock@gmail.com>
2025-11-15 11:54:08 +0100

    Migrate svg_loader

---
+++ src/shared/constants.h
@@ -0,0 +1,30 @@
+//
+// Created by Arthur on 15.11.25.
+//
+
+#ifndef MICROSIMULATION_CONSTANTS_H
+#define MICROSIMULATION_CONSTANTS_H
+
+/* file paths */
+#define SVG_PATH ":/svgs/frames_architecture.svg"			 // can be found
+#define SVG_IDS_PATH ":/svgs/frames_architecture_ids.txt"     // cannot be found
+
+/* icons */
+#define SVG_ICON_SIZE 64
+
+/* styles */
+#define REGISTER_STYLE_SHEET  \
+"padding: 0px;"  			  \
+"margin: 0px;"				  \
+"border: 1px solid grey; "    \
+"border-radius: 2px;"		  \
+"font-weight: bold;" 		  \
+"font-size: 8px; " 		  \
+"color: black;"				  \
+"background: #f0f0f0;"
+
+
+#define MAX_CLOCK_FREQUENCE 50
+
+
+#endif //MICROSIMULATION_CONSTANTS_H
---
+++ src/shared/svg_loader.h
@@ -0,0 +1,39 @@
+//
+// Created by Arthur on 15.11.25.
+//
+
+#ifndef MICROSIMULATION_SVG_LOADER_H
+#define MICROSIMULATION_SVG_LOADER_H
+#include <string>
+#include <QtCore/qstring.h>
+#include <QtGui/qpixmap.h>
+#include <QtGui/qsvgrenderer.h>
+
+namespace shared {
+    class svg_loader {
+        public:
+        static svg_loader & get_instance() {
+            static svg_loader * instance = new svg_loader{};
+            return *instance;
+        }
+
+        void load_from_file(QString const &file_path);
+        QPixmap get_pixmap_for_id(QString const & id, QSize const & size) const;
+        QSvgRenderer & get_renderer();
+        static QRect get_bounds_for_id(const QString &);
+
+    private:
+        svg_loader();
+        ~svg_loader() = default;
+
+        svg_loader(svg_loader const&) = delete;
+        svg_loader& operator=(svg_loader const&) = delete;
+
+        QString m_file_path;
+        QSvgRenderer * m_renderer;
+    };
+
+    };
+} // core
+
+#endif //MICROSIMULATION_SVG_LOADER_H
